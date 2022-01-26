2149ba8d642d1c0cac6b16961c08621f33ab6555
Freddy Clavijo <freddy.clavijo@gmail.com>
2022-01-26 17:44:50 +1100

    Refactor: Update Navbar

--- src/components/molecules/Navbar.jsx
+++ src/components/molecules/Navbar.jsx
@@ -1,43 +1,24 @@
 import React, { useState } from 'react';
+import Logo from '../../assets/images/logo.png';
+import '../../App.css';
 
 const Navbar = () => {
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isMenuExpanded, setIsMenuExpanded] = useState(false);
-
-  // const expandMenu = () => {
-  //   let showHideMenu = state.showHideMenu;
-  //   showHideMenu = (showHideMenu === "hidden") ? "show" : "hidden";
-  //   setState({ showHideMenu })
-  //   console.log(showHideMenu);
-  // }
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   }
 
-  // const toggleMenu = () => {
-  //   let { isExpanded } = state;
-  //   let newExpanded = isExpanded ? false : true
-  //   setState({ isExpanded: newExpanded });
-  //   console.log("newExpanded: ", newExpanded);
-  // }
-
-  const toggleExpandMenu = () => {
-    setIsMenuExpanded(!isMenuExpanded);
-  }
-
-
-  // let { isExpanded } = state;
-  return (<React.Fragment>
+  return (<>
     <header>
       <section id="wrapper">
         <div id="logo">
-          {/* <img src={Logo} alt="logo" /> */}
+          <img src={Logo} alt="logo" />
         </div>
         <div id="title">
-          {/* <img src={Logo} alt="logo" /> */}
-          <a href="#" >Freddy | Developer</a>
+          <img src={Logo} alt="logo" />
+          <p>{`Freddy | Developer`}</p>
         </div>
         <nav id="menu-max">
           <ul>
@@ -46,23 +27,16 @@ const Navbar = () => {
             <li><a href="#contact">Contact</a></li>
           </ul>
         </nav>
-
-        <div id="hamburguer-menu"
-          className={isMenuExpanded ? " open" : " collapsed"}
-          // data-bs-toggle="collapse"
-          // data-bs-target="#navbarCollapse"
-          // aria-controls="navbarCollapse"
-          // aria-expanded="false"
-          // aria-label="Toggle navigation"
+        <div id="hamburger-menu"
+          className={isMenuOpen ? " open" : " collapsed"}
           data-bs-toggle="collapsed"
           data-bs-target="#menu-min-options"
           aria-controls="menu-min-options"
-          aria-expanded={isMenuExpanded}
+          aria-expanded={isMenuOpen}
           aria-label="Toggle navigation"
           onClick={({ target }) => {
             target.classList.toggle('open');
-            console.log("target: ", target);
-            toggleExpandMenu()
+            toggleMenu()
           }}
         >
           <span></span>
@@ -70,21 +44,18 @@ const Navbar = () => {
           <span></span>
           <span></span>
         </div>
-
       </section>
     </header>
     <div id="shadow"></div>
-    <div id="hidder"></div>
-    <div className="clearfix"></div>
-    <nav id="menu-min-options" className={isMenuExpanded ? " show" : " hidden"}>
+    <div id="hidden"></div>
+    <nav id="menu-min-options" className={isMenuOpen ? " show" : " hidden"}>
       <ul>
         <li onClick={toggleMenu}><a href="#aboutMe">About Me</a></li>
         <li onClick={toggleMenu}><a href="#projects">Projects</a></li>
         <li onClick={toggleMenu}><a href="#contact">Contact</a></li>
       </ul>
     </nav>
-    <div className="clearfix"></div>
-  </React.Fragment>)
+  </>)
 }
 
 export default Navbar;
